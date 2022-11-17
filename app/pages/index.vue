41fd91ceae70ab0af818fd4fad5e9e18301f3319
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-11-17 12:23:42 -0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,12 +3,7 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-        <div style="padding:75% 0 0 0;position:relative;">
-          <iframe src="https://player.vimeo.com/video/772171476?h=1bb1e0b002&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay;     fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Carol">
-          </iframe>
-        </div>
-          <script src="https://player.vimeo.com/api/player.js">
-          </script>
+<div style="padding:75% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/772171476?h=1bb1e0b002&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Carol"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
       </div>
     </div>
   </section>
