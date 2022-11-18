9d97dc78da22219da709a49a29f0f51d7a41bb24
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-11-18 13:23:31 -0800

    Add files via upload

---
+++ app/pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="home">
+    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
+        <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
+		<div>
+<iframe src="https://player.vimeo.com/video/772175998?h=dfaf62ae3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1358" height="1080" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Carol (1080p)"></iframe>
+      </div>
+	  </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import settings from '@/content/settings/general.json';
+
+@Component({
+  // Called to know which transition to apply
+  transition() {
+    return 'slide-left';
+  },
+})
+export default class Home extends Vue {
+  welcomeText = settings.welcomeText;
+
+  get posts(): Post[] {
+    return this.$store.state.posts;
+  }
+
+  isSignedUp = false;
+
+  form = {
+    email: '',
+  };
+
+  encode(data): string {
+    return Object.keys(data)
+      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&');
+  }
+
+  validEmail(email): boolean {
+    // eslint-disable-next-line
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  async handleSubmit(): Promise<void> {
+    if (!this.validEmail(this.form.email)) {
+      this.$refs.emailInput.focus();
+      return;
+    }
+
+    try {
+      await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({ 'form-name': 'signups', ...this.form }),
+      });
+
+      this.isSignedUp = true;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+</script>
