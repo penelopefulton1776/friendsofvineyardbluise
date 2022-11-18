0df8daa155c895ea668010a61ae0044d6f894748
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-11-18 14:12:19 -0800

    Update header.vue

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -6,7 +6,7 @@
       </nuxt-link>
     </div>
 
-    <nav class="nav ml-auto">
+    /*<nav class="nav ml-auto">
       <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
         <li>
           <nuxt-link to="/blog" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
@@ -22,7 +22,7 @@
           <nuxt-link :to="`/${page.slug}`">{{ page.title }}</nuxt-link>
         </li>
       </ul>
-    </nav>
+    </nav> */
   </header>
 </template>
 
