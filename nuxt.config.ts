76ec7d16932aa412cb1d6b19dbef8c5fc8c1ec1f
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-11-18 14:12:49 -0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,7 +59,7 @@ const nuxtConfig: Configuration = {
     subFolders: false,
 
     routes: [
-      ...fg.sync([/*'./app/content/blog/**.json', */'./app/content/pages/**.json']).map(url => ({
+      ...fg.sync(['./app/content/blog/**.json', './app/content/pages/**.json']).map(url => ({
         route: url.replace(/^.\/app\/content(\/pages)?|.json$/gi, ''),
         payload: require(url),
       })),
