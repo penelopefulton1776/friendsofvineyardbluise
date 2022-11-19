2a13f7728f5483d22df65d2b734152493256d13d
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-11-19 12:09:01 -0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -33,7 +33,17 @@
 <p style='margin:0in;margin-bottom:.0001pt;font-size:16px;font-family:"Calibri",sans-serif;'>Any questions should be directed to Robert H. Tyler, of Tyler &amp; Bursch, LLP at rtyler@tylerbursch.com</p>
         <div style="padding:79.53% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/772175998?h=dfaf62ae3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Carol (1080p)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 	       <p style='margin:0in;margin-bottom:.0001pt;font-size:16px;font-family:"Calibri",sans-serif;'>&nbsp;</p>      
-        <embed src="/files/002 2022.11.10 Complaint.pdf" width="1440px" height="900px">
+        <nav>
+      <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
+        <li>
+          <nuxt-link to="/content/files/002 2022.11.10 Complaint.pdf" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
+            The Complaint
+          </nuxt-link>
+        </li>
+
+        
+      </ul>
+    </nav>
        
     </div>
     </div>
