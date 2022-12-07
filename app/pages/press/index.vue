dd7cedd461ee01e1d1f66986e2dff4d3d166aacf
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-12-07 09:23:13 -0800

    Update index.vue

--- app/pages/press/index.vue
+++ app/pages/press/index.vue
@@ -18,7 +18,7 @@
             <img
               :alt="post.title"
               class="w-full h-64"
-              :src="post.featuredImage || 'https://source.unsplash.com/random/640x340'"
+              :src="post.featuredImage"
             />
             <div class="p-6 bg-white">
               <h2 class="text-2xl mb-2">{{ post.title }}</h2>
