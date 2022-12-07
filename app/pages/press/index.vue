e5aaf07576391c29549a4276b8dfaf5187eba314
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-12-07 09:29:13 -0800

    Update index.vue

--- app/pages/press/index.vue
+++ app/pages/press/index.vue
@@ -16,7 +16,7 @@
         <div class="post rounded-md overflow-hidden shadow-lg">
           <nuxt-link :to="`/press/${post.slug}`">
             <img
-              :alt=""
+              :alt="post.title"
               class="w-full h-64"
               :src="post.featuredImage"
             />
