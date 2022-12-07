35de0f8043074fd30e4c472f5ea9e1f10ea2c60f
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-12-07 10:02:02 -0800

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,7 +1,26 @@
 <template>
   <footer class="footer -mx-4 md:mx-0">
     <div class="container mx-auto pb-6 flex justify-center">
-      <a href="https://github.com/Gomah/bluise">
+      
+    <nav class="nav ml-auto">
+      <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
+        <li>
+        <a href="/files/002 2022.11.10 Complaint.pdf">The Complaint</a>
+        </li>
+        <li>
+          <nuxt-link to="/press" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
+            Press
+          </nuxt-link>
+        </li>
+        <li
+          v-for="(page, index) in pages"
+          :key="index"
+          class="block font-medium px-4 py-1 md:p-2 lg:px-4"
+        >
+          <nuxt-link :to="`/${page.slug}`">{{ page.title }}</nuxt-link>
+        </li>
+      </ul>
+    </nav>
         <svg
           baseProfile="tiny"
           height="32px"
