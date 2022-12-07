84dc9ca794135b2e2fedba4a944c098c2c6f8e6a
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2022-12-07 10:08:50 -0800

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,24 +1,26 @@
 <template>
   <footer class="footer -mx-4 md:mx-0">
-    <nav class="nav ml-auto">
-      <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
+   <div class="container mx-auto pb-6 flex justify-center">
+      <nav class="nav ml-auto">
+       <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
         <li>
-        <a href="/files/002 2022.11.10 Complaint.pdf">The Complaint</a>
-        </li>
-        <li>
-          <nuxt-link to="/press" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
-            Press
-          </nuxt-link>
-        </li>
-        <li
+         <a href="/files/002 2022.11.10 Complaint.pdf">The Complaint</a>
+         </li>
+         <li>
+            <nuxt-link to="/press" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
+              Press
+            </nuxt-link>
+          </li>
+         <li
           v-for="(page, index) in pages"
           :key="index"
           class="block font-medium px-4 py-1 md:p-2 lg:px-4"
-        >
+          >
           <nuxt-link :to="`/${page.slug}`">{{ page.title }}</nuxt-link>
-        </li>
-      </ul>
-    </nav>
+         </li>
+        </ul>
+      </nav>
+    </div>
   </footer>
 </template>
 
