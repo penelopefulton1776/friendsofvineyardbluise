528a3cf125f33a7b7c76656dbdbac558d2e94b8e
penelopefulton1776 <118391986+penelopefulton1776@users.noreply.github.com>
2023-07-27 20:16:09 -0700

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-<p style="margin: 0in 0in 0.0001pt; font-size: 16px; font-family: &quot;Calibri&quot;, sans-serif;">&nbsp;</
+<p style="margin: 0in 0in 0.0001pt; font-size: 16px; font-family: &quot;Calibri&quot;, sans-serif;">&nbsp;</p>
 <p style="margin: 0in 0in 0.0001pt; font-size: 20px; font-family: &quot;Calibri&quot;, sans-serif;"><b><i>"We're in a declared war, but unless were clear a bout who the enemy is, we'll waste our time fighting enemies that aren't enemies at all. There's only one enemy and no matter what people do, say or react people are never the enemy. The enemy is our only enemy..."</i></b></p>
 <p style="margin: 0in 0in 0.0001pt; font-size: 20px; font-family: &quot;Calibri&quot;, sans-serif;"><b><i>-John Wimber</i></b></p>
 <iframe frameborder="0" scrolling="no" width="100" height="100"
